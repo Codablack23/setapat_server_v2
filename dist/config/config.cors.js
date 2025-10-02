1f444ed35f5aa654a6b7c4bfcba0285add83a216
Edih Goodluck <goodluckedih@yahoo.com>
2025-10-02 17:53:42 +0100

    added setapat subdomains to cors

--- dist/config/config.cors.js
+++ dist/config/config.cors.js
@@ -1,11 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsOptions = void 0;
-const CORS_WHITELIST = ['https://www.setapat.com'];
+const CORS_WHITELIST = ['https://www.setapat.com', 'https://beta.setapat.com'];
 const validateCors = (origin, callback) => {
     if (!origin)
         return callback(null, true);
     if (origin.includes('localhost'))
+        return callback(null, true);
+    const url = new URL(origin);
+    if (url.hostname.endsWith('.setapat.com'))
         return callback(null, true);
     if (CORS_WHITELIST.includes(origin))
         return callback(null, true);
--- src/config/config.cors.ts
+++ src/config/config.cors.ts
@@ -3,7 +3,7 @@ import {
   CustomOrigin,
 } from '@nestjs/common/interfaces/external/cors-options.interface';
 
-const CORS_WHITELIST = ['https://www.setapat.com'];
+const CORS_WHITELIST = ['https://www.setapat.com', 'https://beta.setapat.com'];
 
 const validateCors: CustomOrigin = (origin, callback) => {
   // Allow requests with no origin (e.g., Postman, server-to-server)
@@ -12,7 +12,11 @@ const validateCors: CustomOrigin = (origin, callback) => {
   // Allow localhost during development
   if (origin.includes('localhost')) return callback(null, true);
 
-  // Allow origins in the whitelist
+  // Allow all subdomains of setapat.com
+  const url = new URL(origin);
+  if (url.hostname.endsWith('.setapat.com')) return callback(null, true);
+
+  // Allow exact origins in the whitelist
   if (CORS_WHITELIST.includes(origin)) return callback(null, true);
 
   // Reject any other origins
@@ -21,6 +25,6 @@ const validateCors: CustomOrigin = (origin, callback) => {
 
 export const corsOptions: CorsOptions = {
   origin: validateCors,
-  credentials: true, // allow cookies or auth headers
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'], // explicitly allowed methods
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
 };
